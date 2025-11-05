3d0a72a624b1e004506a3a33f6d804e7fbf78a9d
marsya505 <marsyazulfazahrani0@gmail.com>
2025-11-05 21:00:28 +0700

    Polymorphic Arguments & instanceOf

--- jobsheet11/PegawaiDemo11.java
+++ jobsheet11/PegawaiDemo11.java
@@ -1,17 +1,19 @@
 public class PegawaiDemo11 {
     public static void main(String[] args) {
         Dosen11 dosen1 = new Dosen11("19940201", "Widia, S.Kom. M.Kom", "199420");
-        
-        Pegawai11 pegawai1 = dosen1;
+        TenagaKependidikan11 tendik1 = new TenagaKependidikan11("19750301", "Aida, A.Md.", "Tenaga Administrasi");
 
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
+        train(dosen1);
+        train(tendik1);
+    }
 
-        Dosen11 newDosen = (Dosen11) pegawai1;
+    public static void train(Pegawai11 pegawai) {
+        pegawai.displayInfo();
+        System.out.println("Mengenalkan lingkungan kampus");
+        System.out.println("Menginfokan SOP/Juknis");
 
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+        if (pegawai instanceof Dosen11) {
+            System.out.println("Memberikan pelatihan pedagogik");
+        }
     }
 }
